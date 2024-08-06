ca4379cf3ebe2591d2655b132a919df94f25e02e
Axel Rojas Bachem <git@simplyroba.de>
2024-08-06 21:06:07 +0200

    fix: database connection (#4)

--- src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
+++ src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
@@ -1,5 +1,17 @@
 package de.simplyroba.suite.budgets
 
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 
-@TestConfiguration(proxyBeanMethods = false) class TestcontainersConfiguration
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+  @Bean
+  @ServiceConnection
+  fun postgresContainer(): PostgreSQLContainer<*> {
+    return PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
+  }
+}
