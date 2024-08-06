623bc47d0cd37b74bb29e60d2126f2160d538d94
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2024-08-06 21:03:47 +0200

    format

--- src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
+++ src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
@@ -9,10 +9,9 @@ import org.testcontainers.utility.DockerImageName
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-    @Bean
-    @ServiceConnection
-    fun postgresContainer(): PostgreSQLContainer<*> {
-        return PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
-    }
-
+  @Bean
+  @ServiceConnection
+  fun postgresContainer(): PostgreSQLContainer<*> {
+    return PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
+  }
 }
