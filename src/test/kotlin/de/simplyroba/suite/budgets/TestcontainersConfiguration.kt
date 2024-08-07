f8b547a80c264df8c880e8fa9a91e5b27cab6bbd
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2024-08-07 11:13:26 +0200

    format

--- src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
+++ src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
@@ -1,12 +1,9 @@
 package de.simplyroba.suite.budgets
 
-import java.time.Duration
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.containers.wait.strategy.Wait
-import org.testcontainers.containers.wait.strategy.WaitAllStrategy
 import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
