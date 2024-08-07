7a5213fc15a5500ee98b13b9c839a45bc6338d72
Axel Rojas Bachem <git@simplyroba.de>
2024-08-07 11:25:50 +0200

    chore: use PG 16 alpine (#6)

--- src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
+++ src/test/kotlin/de/simplyroba/suite/budgets/TestcontainersConfiguration.kt
@@ -1,9 +1,12 @@
 package de.simplyroba.suite.budgets
 
+import java.time.Duration
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy
 import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,6 +15,16 @@ class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
   fun postgresContainer(): PostgreSQLContainer<*> {
-    return PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
+    return PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+      // see
+      // https://github.com/rancher-sandbox/rancher-desktop/issues/2609#issuecomment-1788871956
+      .waitingFor(
+        WaitAllStrategy()
+          .withStrategy(Wait.forListeningPort())
+          .withStrategy(
+            Wait.forLogMessage(".*database system is ready to accept connections.*\\s", 2)
+              .withStartupTimeout(Duration.ofSeconds(60))
+          )
+      )
   }
 }
