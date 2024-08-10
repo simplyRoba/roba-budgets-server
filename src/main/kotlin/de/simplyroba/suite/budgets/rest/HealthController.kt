f1ce83e8e6aab7996a002cbb5757ee6735168cd0
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2024-08-10 21:17:39 +0200

    add health controller and docker file

--- src/main/kotlin/de/simplyroba/suite/budgets/rest/HealthController.kt
+++ src/main/kotlin/de/simplyroba/suite/budgets/rest/HealthController.kt
@@ -1,13 +1,15 @@
 package de.simplyroba.suite.budgets.rest
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
+@RequestMapping("/health")
 class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/check")
     fun health(): Mono<Unit> {
         return Mono.empty()
     }
--- src/test/kotlin/de/simplyroba/suite/budgets/integration/HealthIntegrationTest.kt
+++ src/test/kotlin/de/simplyroba/suite/budgets/integration/HealthIntegrationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class HealthIntegrationTest : AbstractIntegrationTest() {
     @Test
     fun `health endpoint should return 200`() {
-         webTestClient.get().uri("/health")
+         webTestClient.get().uri("/health/check")
              .exchange()
              .expectStatus()
              .isOk
