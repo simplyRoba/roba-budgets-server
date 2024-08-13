60e9e5afdf9fa362ea97bcd54cdc188a36eabb3f
Axel Rojas Bachem <axel.rojasbachem@rewe-group.com>
2024-08-13 13:45:40 +0200

    fix: wrong column name

--- src/main/resources/db/changelog/scripts/01_base_tables.sql
+++ src/main/resources/db/changelog/scripts/01_base_tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE income
     id              INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title           VARCHAR NOT NULL,
     amount_in_cents INT     NOT NULL,
-    dueDate         DATE    NOT NULL
+    due_date         DATE    NOT NULL
 );
 
 CREATE TABLE income_template
@@ -44,7 +44,7 @@ CREATE TABLE expense
     budget_id       INT REFERENCES budget (id)
 );
 
-CREATE INDEX income_date_idx ON income (date);
+CREATE INDEX income_date_idx ON income (due_date);
 CREATE INDEX expense_due_date_idx ON expense (due_date);
 CREATE INDEX expense_type_idx ON expense (type);
 CREATE INDEX expense_category_id_idx ON expense (category_id);
